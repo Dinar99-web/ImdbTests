b8058ce910f539ff53c0b25f4abe8dd24a2364c4
Dinar <19yakudza99@gmail.com>
2025-07-23 23:57:20 +0500

    tvEpisodesSearchTest was added

--- src/test/java/pages/SearchResultsPage.java
+++ src/test/java/pages/SearchResultsPage.java
@@ -2,7 +2,6 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$$;
